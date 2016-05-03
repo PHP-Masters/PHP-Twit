5f63c5c3f99e8abc02572715ffad9862db7fdd65
juliansamek <julian.samek@ucc.on.ca>
2016-05-03 15:26:44 -0400

    Makes register page work

--- signin.php
+++ signin.php
@@ -12,6 +12,56 @@
 			<a class="btn btn-primary-outline pull-xs-right" href="index.php">Main Page</a>
 		</nav>
 
+        <div class="container-fluid">
+            <div class="card-deck-wrapper"><div class="card-deck">
+                <div class="card">
+                    <h3 class="card-header bg-info text-xs-center">Login</h3>
+					<form action="signin.php" method="post">
+	                    <div class="card-block">
+	                        <fieldset class="form-group">
+	                            <label for="team-name">User Name</label>
+	                            <input class="form-control" name="username" placeholder="ex. John Smith" value="<?php echo $submitted_username; ?>">
+	                        </fieldset>
+
+	                        <fieldset class="form-group">
+	                            <label for="password">Password</label>
+	                            <input class="form-control" name="password" placeholder="ex. Password" value="">
+	                        </fieldset>
+	                    </div>
+	                    <div class="card-footer bg-faded text-xs-center" id="login-btn">
+	                        <input class="btn btn-info" type="submit" value="Login"/>
+	                    </div>
+					</form>
+                </div>
+
+                <div class="card">
+                    <h3 class="card-header bg-info text-xs-center">Register</h3>
+					<form action="signin.php" method="post">
+	                    <div class="card-block">
+	                        <fieldset class="form-group">
+	                            <label for="email">Email Address</label>
+	                            <input class="form-control" name="email" placeholder="ex. john.smith@gmail.com">
+	                            <small class="text-muted">We'll never share your email with anyone else.</small>
+	                        </fieldset>
+
+	                        <fieldset class="form-group">
+	                            <label for="Team Name">User Name</label>
+	                            <input class="form-control" name="username" placeholder="ex. John Smith">
+	                        </fieldset>
+
+	                        <fieldset class="form-group">
+	                            <label for="password">Password</label>
+	                            <input class="form-control" name="password" placeholder="ex. Password">
+	                        </fieldset>
+	                    </div>
+	                    <div class="card-footer bg-faded text-xs-center">
+	                        <input class="btn btn-info" type="submit" value="Register"/>
+	                    </div>
+					</form>
+                </div>
+            </div></div>
+        </div>
+
 		<?php
 			require("common.php");
 			$submitted_username = '';
@@ -59,56 +109,121 @@
 					$submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8');
 				}
 			}
+
+			/////////
+			// register
+			////////
+
+			// First we execute our common code to connection to the database and start the session
+		    require("common.php");
+
+		    // This if statement checks to determine whether the registration form has been submitted
+		    // If it has, then the registration code is run, otherwise the form is displayed
+		    if(!empty($_POST))
+		    {
+		        // Ensure that the user has entered a non-empty username
+		        if(empty($_POST['username']))
+		        {
+		            // Note that die() is generally a terrible way of handling user errors
+		            // like this.  It is much better to display the error with the form
+		            // and allow the user to correct their mistake.  However, that is an
+		            // exercise for you to implement yourself.
+		            die("Please enter a username.");
+		        }
+
+		        // Ensure that the user has entered a non-empty password
+		        if(empty($_POST['password']))
+		        {
+		            die("Please enter a password.");
+		        }
+
+		        // Make sure the user entered a valid E-Mail address
+		        // filter_var is a useful PHP function for validating form input, see:
+		        // http://us.php.net/manual/en/function.filter-var.php
+		        // http://us.php.net/manual/en/filter.filters.php
+		        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		            die("Invalid E-Mail Address");
+		        }
+
+		        $query = "
+		            SELECT
+		                1
+		            FROM users
+		            WHERE
+		                username = :username
+		        ";
+
+		        $query_params = array(
+		            ':username' => $_POST['username']
+		        );
+
+		        try {
+		            // These two statements run the query against your database table.
+		            $stmt = $db->prepare($query);
+		            $result = $stmt->execute($query_params);
+		        } catch(PDOException $ex) {
+		            // Note: On a production website, you should not output $ex->getMessage().
+		            // It may provide an attacker with helpful information about your code.
+		            die("Failed to run query: " . $ex->getMessage());
+		        }
+		        $row = $stmt->fetch();
+    			if($row) {
+		            die("This username is already in use");
+		        }
+		        $query = "
+		            SELECT
+		                1
+		            FROM users
+		            WHERE
+		                email = :email
+		        ";
+		        $query_params = array(
+		            ':email' => $_POST['email']
+		        );
+		        try {
+		            $stmt = $db->prepare($query);
+		            $result = $stmt->execute($query_params);
+		        } catch(PDOException $ex) {
+		            die("Failed to run query: " . $ex->getMessage());
+		        }
+		        $row = $stmt->fetch();
+		        if($row) {
+		            die("This email address is already registered");
+		        }
+		        $query = "
+		            INSERT INTO users (
+		                username,
+		                password,
+		                salt,
+		                email
+		            ) VALUES (
+		                :username,
+		                :password,
+		                :salt,
+		                :email
+		            )
+		        ";
+
+		        $salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647));
+		        $password = hash('sha256', $_POST['password'] . $salt);
+		        for($round = 0; $round < 65536; $round++) {
+		            $password = hash('sha256', $password . $salt);
+		        }
+				$query_params = array (
+		            ':username' => $_POST['username'],
+		            ':password' => $password,
+		            ':salt' => $salt,
+		            ':email' => $_POST['email']
+		        );
+		        try {
+		            // Execute the query to create the user
+		            $stmt = $db->prepare($query);
+		            $result = $stmt->execute($query_params);
+		        } catch(PDOException $ex) {
+		            die("Failed to run query: " . $ex->getMessage());
+		        }
+		        header("Location: signin.php");
+		    }
 		?>
-
-        <div class="container-fluid">
-            <div class="card-deck-wrapper"><div class="card-deck">
-                <div class="card">
-                    <h3 class="card-header bg-info text-xs-center">Login</h3>
-					<form action="signin.php" method="post">
-	                    <div class="card-block">
-	                        <fieldset class="form-group">
-	                            <label for="team-name">Team Name</label>
-	                            <input class="form-control" name="username" placeholder="ex. John Smith" value="<?php echo $submitted_username; ?>">
-	                        </fieldset>
-
-	                        <fieldset class="form-group">
-	                            <label for="password">Password</label>
-	                            <input class="form-control" name="password" placeholder="ex. Password" value="">
-	                        </fieldset>
-	                    </div>
-	                    <div class="card-footer bg-faded text-xs-center" id="login-btn">
-	                        <input class="btn btn-info" type="submit" value="Login"/>
-	                    </div>
-					</form>
-                </div>
-
-				
-
-                <div class="card">
-                    <h3 class="card-header bg-info text-xs-center">Register</h3>
-                    <div class="card-block">
-                        <fieldset class="form-group">
-                            <label for="email">Email Address</label>
-                            <input class="form-control" id="email" placeholder="ex. john.smith@gmail.com">
-                            <small class="text-muted">We'll never share your email with anyone else.</small>
-                        </fieldset>
-
-                        <fieldset class="form-group">
-                            <label for="Team Name">Team Name</label>
-                            <input class="form-control" id="team-name" placeholder="ex. John Smith">
-                        </fieldset>
-
-                        <fieldset class="form-group">
-                            <label for="password">Password</label>
-                            <input class="form-control" id="team-name" placeholder="ex. Password">
-                        </fieldset>
-                    </div>
-                    <div class="card-footer bg-faded text-xs-center">
-                        <a class="btn btn-info">Register</a>
-                    </div>
-                </div>
-            </div></div>
-        </div>
     </body>
 </html>
