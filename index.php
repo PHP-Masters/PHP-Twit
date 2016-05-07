f304cd016c10bf55659dab63a39a365a9de7716e
juliansamek <julian.samek@ucc.on.ca>
2016-05-07 15:52:21 -0400

    Renames home.php in order to create an actual website like

--- index.php
+++ index.php
@@ -2,17 +2,16 @@
     <head>
         <link rel="stylesheet" href="Resources/CSS/bootstrap.css"/>
         <link rel="stylesheet" href="Resources/CSS/style.css"/>
-        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 		<script src="Resources/JS/jquery.js"/></script>
 		<script src="Resources/JS/bootstrap.js"/></script>
-        <script src="Resources/JS/style.js"/></script>
+        <script src="Resources/JS/script.js"/></script>
     </head>
 
     <body>
         <nav class="navbar navbar-light bg-faded" id="navbar-main">
 			<a class="navbar-brand" href="home.php">Twitter</a>
 			<a class="btn btn-primary-outline pull-xs-right" id="register-btn" href="register.php">Register</a>
-      <a class="btn btn-primary-outline pull-xs-right" href="login.php">Login</a>
+            <a class="btn btn-primary-outline pull-xs-right" href="login.php">Login</a>
 		</nav>
     </body>
 </html>
