5136241bc70002d4d9f80dd5e59932d7ce80085c
Simon Osak <simon.osak@ucc.on.ca>
2016-05-09 13:56:08 -0400

    Added more things under dropdown

--- home.php
+++ home.php
@@ -13,11 +13,30 @@
 			<a class="btn btn-primary-outline pull-xs-right" id="register-btn" href="register.php">Register</a>
             <a class="btn btn-primary-outline pull-xs-right" href="login.php">Login</a>
 		</nav>
+
         <div class="center">
             <h1 class="text-primary"> Welcome to Our Site </h1>
             <p> This is our beautiful, intuitive and unique twitter website.  We would go as far as saying it's better than twitter itself!
             </br> Check this cool stuff out!
             </p>
         </div>
+
+        <div class="btn-group btn-block">
+            <button type="button" class="btn btn-block btn-primary dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog"></span> Creators <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" role="menu">
+              <p> <center> <b> Julian </b>
+              <br>
+              Julian is sick.<br> Absolute legend. <br> Straight Savage. <br> Obama out.
+              </center>
+            </ul>
+            <ul class="dropdown-menu1" role="menu">
+              <p> <center> <b> Simon </b>
+              <br>
+              Simon is a cow.<br> He looks like a fat elephant. <br> But he's also a straight savage. <br> Mic Drop.
+              </center>
+            </ul>
+          </div>
+
     </body>
 </html>
