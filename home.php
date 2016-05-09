c5f714e4b477463c7244d42eb3d5811ab5c0578d
Simon Osak <simon.osak@ucc.on.ca>
2016-05-09 13:17:46 -0400

    Added bootraped text on home page

--- home.php
+++ home.php
@@ -10,9 +10,17 @@
 
     <body>
         <nav class="navbar navbar-light bg-faded" id="navbar-main">
-			<a class="navbar-brand" href="home.php">Twitter</a>
+			<a class="navbar-brand" href="home.php"> Twitter </a>
 			<a class="btn btn-primary-outline pull-xs-right" id="register-btn" href="register.php">Register</a>
             <a class="btn btn-primary-outline pull-xs-right" href="login.php">Login</a>
 		</nav>
+    <div>
+    <h1> <center> Welcome to Our Site </center> </h1>
+    <p> This is our beautiful, intuitive and unique twitter website.  We would go as far as saying it's better than twitter itself!
+    </br> Check this cool stuff out!
+    </p>
+
+  </div>
+
     </body>
 </html>
