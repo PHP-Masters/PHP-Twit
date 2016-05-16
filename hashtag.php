4169b459e824ebe6d6d89775cea8fcd3521723e8
caasig123 <ngisaac01@gmail.com>
2016-05-16 12:16:51 -0400

    Now it works

--- hashtag.php
+++ hashtag.php
@@ -29,16 +29,9 @@
 		</nav>
 
         <?php
-<<<<<<< HEAD
             date_default_timezone_get('America/Toronto');
             $date = date('Y-m-d');
             $current_time_now = time();
-=======
-            // set the time zone to EST
-            date_default_timezone_set('America/Toronto');
-            // return the date 
-            $date = date('Y-m-d H:i:s');
->>>>>>> origin/master
 
             require("common.php");
             if(empty($_SESSION['user'])) {
