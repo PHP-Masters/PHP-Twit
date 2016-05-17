f8ec0c53765da860d48eeedab8218cc9ce0f0b45
juliansamek <julian.samek@ucc.on.ca>
2016-05-17 10:30:54 -0400

    Updates the sql code

--- import.sql
+++ import.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: May 17, 2016 at 12:06 AM
+-- Generation Time: May 17, 2016 at 04:30 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -31,7 +31,7 @@ CREATE TABLE `symbols` (
   `date` date NOT NULL,
   `time` int(11) NOT NULL,
   `hours` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `symbols`
@@ -52,17 +52,18 @@ INSERT INTO `symbols` (`id`, `author`, `post`, `hashtags`, `tags`, `likes`, `dis
 (45, '@asd', '', '', '@julian', 1, 0, '2016-05-06', 0, 0),
 (46, '@Julian', 'Wow, the dates work', '#newpost #twohashtags', '@asd', 2, 0, '2016-05-07', 0, 0),
 (48, '@Julian', '', '#hello', '', 0, 0, '2016-05-07', 0, 0),
-(49, '@Julian', '', '#hello #hey', '', 3, 0, '2016-05-07', 0, 0),
-(52, '@Julian', 'Hello #autohashtags - this works well', '#autohashtags ', '', 0, 0, '2016-05-07', 0, 0),
+(49, '@Julian', '', '#hello #hey', '', 4, 0, '2016-05-07', 0, 0),
+(52, '@Julian', 'Hello #autohashtags - this works well', '#autohashtags ', '', 1, 0, '2016-05-07', 0, 0),
 (55, '@Julian', 'It''s my very own tweet #tweets @asd @test', '#tweets ', '@asd @test ', 2, 1, '2016-05-07', 0, 0),
-(56, '@julian', 'The new error #error messages look so good @julian', '#error ', '@julian ', 4, 3, '2016-05-07', 0, 0),
-(58, '@test', '"Hello"', '', '', 9, 2, '2016-05-08', 0, 0),
-(59, '@test', 'Hello #test', '#test ', '', 24, 13, '2016-05-09', 0, 0),
-(60, '@test', 'Testing the time #testing', '#testing ', '', 15, 3, '2016-05-11', 9, 0),
-(61, '@test', 'This shows time and date', '', '', 8, 16, '2016-05-12', 38, 0),
+(56, '@julian', 'The new error #error messages look so good @julian', '#error ', '@julian ', 8, 3, '2016-05-07', 0, 0),
+(58, '@test', '"Hello"', '', '', 10, 3, '2016-05-08', 0, 0),
+(59, '@test', 'Hello #test', '#test ', '', 25, 13, '2016-05-09', 0, 0),
+(60, '@test', 'Testing the time #testing', '#testing ', '', 15, 5, '2016-05-11', 9, 0),
+(61, '@test', 'This shows time and date', '', '', 9, 16, '2016-05-12', 38, 0),
 (62, '@test', 'Testing the time', '', '', 2, 8, '2016-05-12', 26, 0),
-(63, '@test', 'Also testing the time function', '', '', 0, 0, '2016-05-16', 1463400104, 0),
-(64, '@test', 'Testing the time #testing', '#testing ', '', 4, 0, '2016-05-16', 1463424636, 0);
+(63, '@test', 'Also testing the time function', '', '', 1, 0, '2016-05-16', 1463400104, 0),
+(64, '@test', 'Testing the time #testing', '#testing ', '', 6, 0, '2016-05-16', 1463424636, 0),
+(65, '@test', 'One final test for the program #testing @julian', '#testing ', '@julian ', 1, 0, '2016-05-17', 1463493588, 0);
 
 -- --------------------------------------------------------
 
@@ -77,20 +78,21 @@ CREATE TABLE `users` (
   `salt` char(16) NOT NULL,
   `email` varchar(225) NOT NULL,
   `bio` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `salt`, `email`, `bio`) VALUES
-(1, 'test', '68b1b2f150f62deff233bd033c3952f620bc524b678af9dc41979ba299432e4e', '203a60d44b5cb80', 'test@gmail.com', 'This is my new bio.'),
+(1, 'test', '68b1b2f150f62deff233bd033c3952f620bc524b678af9dc41979ba299432e4e', '203a60d44b5cb80', 'test@gmail.com', 'This is my new bio. I can write what ever I want!'),
 (2, 'julian', '559fa5822874d2bf64523b19fc397cc3198abc41f59de9d4880a4efd5b086d17', '5370039f319fbe9f', 'julian.samek@ucc.on.ca', 'Hey, I''m Julian and I''m one of the creators of this awesome website.'),
 (3, 'username', '22538fce6eed10bed514ba3e4677b28ebf5f672172e79f20a465c8b99cceb310', '73011553b5210f9', 'username@mail.mail', ''),
 (4, 'adil', '3191462157712ba7fc4c5ea352d94da18d754e073553270c1e508db3f2d6125a', '682873b537a3ede1', 'adil.natal@ucc.on.ca', 'I''m Adil. I''m better than Carson in every way'),
 (5, 'jul', '96b188699bbb3b568d160f14e0ef0a13519d90d84f358e14cdb3a88596701541', '18ab273f2e1f0524', 'julian@gmail.com', ''),
 (6, 'chef_samek', '4e5e68fba0fd407a4d937cfadb62d9584d5d0b2a85a8fe08fbd072dde07d2e6c', '6e4edf826765727f', 'chef@gmail.com', ''),
-(7, 'new', 'a88bcb6f22a94370eb219568e90efcad466ae955ac6a1f51fc0e8f617cf4e9f5', '7235208164782c7', 'new@gmail.com', '');
+(7, 'new', 'a88bcb6f22a94370eb219568e90efcad466ae955ac6a1f51fc0e8f617cf4e9f5', '7235208164782c7', 'new@gmail.com', ''),
+(8, 'Juliana', 'aed742e4be064ca25ed362688f69d346f7aeae485b16d317f6d254cb6d0014c2', '39e5e8e47de7f2c', 'gam@gmail.com', '');
 
 --
 -- Indexes for dumped tables
@@ -116,9 +118,9 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `symbols`
 --
 ALTER TABLE `symbols`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=66;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
